d89cc387b83fa7f8802934e37a92c5b790c7de44
Kudana Lionel Gwatidzo <kudanagwatidzo@gmail.com>
2020-04-08 19:03:14 -0400

    1st Commit: High-level architecture

---
+++ src/main/java/org/zincat/covidmap/CovidmapApplication.java
@@ -0,0 +1,14 @@
+package org.zincat.covidmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidmapApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(CovidmapApplication.class, args);
+	}
+
+}
